ccdaf2dcc9758a3785923761b033f05b6b1d4120
Terry Yin <terry@odd-e.com>
2016-10-04 21:02:33 +0800

    player

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :race_games
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :race_games do
+    resources :race_players, shallow: true
+  end
 end
