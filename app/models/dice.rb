fdc6bce8e1555b9892061b20f12b811740dfd6db
syamamoto-pastoraldog <shimpei@pd-sy.local>
2016-11-22 16:25:09 +0900

    create dice method

--- app/models/dice.rb
+++ app/models/dice.rb
@@ -1,2 +1,15 @@
-class Dice < ApplicationRecord
+class Dice
+  MAX_NUM = 6
+
+  def get_number
+    return rand(MAX_NUM) + 1
+  end
+
+  def calc_normal_step(dice)
+    if dice.odd?
+      return 1
+    else
+      return 2
+    end
+  end
 end
--- spec/models/dice_spec.rb
+++ spec/models/dice_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Dice, type: :model do
-  
+  before do
+    @dice = Dice.new
+  end
+  describe 'get_number' do
+    it 'get dise number' do
+      number = @dice.get_number
+      expect([1, 2, 3, 4, 5, 6]).to include number
+    end
+  end
+
+  describe 'calc_normal_step' do
+    it 'dice is 1 return 1' do
+      step = @dice.calc_normal_step(1)
+      expect(step).to eq 1
+    end
+
+    it 'dice is 4 return 2' do
+      step = @dice.calc_normal_step(4)
+      expect(step).to eq 2
+    end
+  end
 end
