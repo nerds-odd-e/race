8716d17165b442d83eb58391495f2c4308d60bd8
syamamoto-pastoraldog <shimpei@pd-sy.local>
2016-11-22 15:58:06 +0900

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20161122013922) do
+
+  create_table "players", force: :cascade do |t|
+    t.integer  "distance",   default: 0
+    t.integer  "damage",     default: 0
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
 end
