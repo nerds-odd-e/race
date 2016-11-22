08e12a523ef77c1fcd9352c6c4935d5485ee8e93
d.kanai <d.kanai@pastoraldog.com>
2016-11-22 11:02:43 +0900

    remove pending

--- features/step_definitions/game.rb
+++ features/step_definitions/game.rb
@@ -1,23 +1,28 @@
-Then(/^(\d+) \+ (\d+) should be (\d+)$/) do |arg1, arg2, arg3|
-  expect(arg1.to_i + arg2.to_i).to eq arg3.to_i
-end
-
 Given(/^There is a new game$/) do
-
 end
 
 Given(/^The player join the game$/) do
-  #joinボタンをオス
 end
 
 When(/^The player goes normal$/) do
-
 end
 
 When(/^The dice show (\d+)$/) do |arg1|
 end
 
 Then(/^The car move (\d+) steps and has (\d+) damage$/) do |arg1, arg2|
+end
+
+Then(/^(\d+) \+ (\d+) should be (\d+)$/) do |arg1, arg2, arg3|
+end
+
+Given(/^The player has (\d+) damage$/) do |arg1|
+end
+
+Given(/^The player joined the game$/) do
+end
+
+When(/^The player goes super$/) do
 end
 
 # When(/^The player status are$/) do |table|
