82452fb3a0826915951c1edc83d95f6ee39fed74
syamamoto-pastoraldog <shimpei@pd-sy.local>
2016-11-24 16:57:57 +0900

    add next check in Game Controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,8 @@
 class GamesController < ApplicationController
   def big_screen
     @moved = Player.where(throw_flag: true).count  
+    if Game.new.is_move_next?(Player.count, @moved)
+      @moved = 0
+    end
   end
 end
