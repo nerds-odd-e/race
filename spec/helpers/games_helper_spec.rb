f4af19fda200b163f43281d005e6519866c07222
k.kinoshita <k.kinoshita@pastoraldog.com>
2016-11-25 14:00:38 +0900

    refactoring

--- spec/helpers/games_helper_spec.rb
+++ spec/helpers/games_helper_spec.rb
@@ -4,9 +4,8 @@ require 'rails_helper'
 RSpec.describe GamesHelper, type: :helper do
   describe 'views car image' do
     it 'return image tag for views car' do
-      player = FactoryGirl::create :player
+      player = FactoryGirl.create :player
       expect(car_image(player)).to match 'car-scar0.png'
     end
   end
-
 end
