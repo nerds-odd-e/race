8f72df06c3a4f77d04482a6480e8af15ddf09809
Jackson Zhang <zbcjackson@gmail.com>
2026-07-15 17:24:56 +0700

    update schema file from migrate script

--- db/schema.rb
+++ db/schema.rb
@@ -2,40 +2,39 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161016065934) do
-
+ActiveRecord::Schema[7.1].define(version: 2016_10_16_065934) do
   create_table "race_games", force: :cascade do |t|
-    t.integer  "distance",   default: 20, null: false
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
-    t.integer  "tick",       default: 0,  null: false
+    t.integer "distance", default: 20, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "tick", default: 0, null: false
   end
 
   create_table "race_moves", force: :cascade do |t|
-    t.integer  "race_player_id"
-    t.string   "choice"
-    t.integer  "dice_face",      null: false
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-    t.integer  "tick",           null: false
+    t.integer "race_player_id"
+    t.string "choice"
+    t.integer "dice_face", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "tick", null: false
     t.index ["race_player_id"], name: "index_race_moves_on_race_player_id"
   end
 
   create_table "race_players", force: :cascade do |t|
-    t.integer  "race_game_id"
-    t.integer  "steps",        default: 0, null: false
-    t.integer  "old_scars",    default: 0, null: false
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-    t.integer  "next_rand"
+    t.integer "race_game_id"
+    t.integer "steps", default: 0, null: false
+    t.integer "old_scars", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "next_rand"
     t.index ["race_game_id"], name: "index_race_players_on_race_game_id"
   end
 
